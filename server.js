fb9e8ed3b750960d928eaacdc0e9fbcc3e4c5bd8
Takacs Robert <89.t.robert@gmail.com>
2020-05-06 13:31:11 +0200

    feat(): stable version on local, trying on heroku

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-    
-    setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+    setInterval(() => socket.emit('time', new Date().toTimeString()), 1000);
 });
 
 http.listen(3000, () => {
