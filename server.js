cc0105db719107f73e6049109f05e56f009572bc
Takacs Robert <89.t.robert@gmail.com>
2020-05-12 16:44:59 +0200

    fix(): PORT defined as env OR 3000

--- server.js
+++ server.js
@@ -2,15 +2,21 @@ let app = require('express')();
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
 
+let PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log(`A user with ID: ${socket.id} connected.`);
     setInterval(() => socket.emit('time', new Date().toTimeString()), 1000);
+
+    io.on('disconnect', (socket) => {
+        console.log(`A user with ID: ${socket.id} disconnected.`)
+    })
 });
 
-http.listen(3000, () => {
-    console.log('listening on *:3000');
+http.listen(PORT, () => {
+    console.log(`Listening on ${PORT}.`);
 });
