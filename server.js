9fb7367ff8e5cdb2fb71a90240475777bdd6cfb7
Takacs Robert <89.t.robert@gmail.com>
2020-05-13 13:24:32 +0200

    feat(): prepared login() for players

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+const mySql = require('./js/mysql-connect');
+
 // Listening on an open port.
 http.listen(PORT, () => {
     console.log(`Listening on ${PORT}.`);
@@ -23,10 +25,20 @@ app.get('/admin', (req, res) => {
 });
 
 // Setting what should be done when someone connects.
-io.on('connection', (socket) => {
+io.on('connection', socket => {
     console.log(`A user with ID: ${socket.id} connected.`);
 
     socket.on('disconnect', () => {
         console.log(`A user with ID: ${socket.id} disconnected.`)
+    })
+
+    socket.on('login', data => {
+        mySql.connect();
+
+        mySql.query(`SELECT * FROM player WHERE name = '${data.name}' AND password = '${data.password}'`, (error, results, fields) => {
+            console.log(results);
+        });
+
+        mySql.end();
     })
 });
