7065bed77a7e04e07a6a0cf46592fd88653b681b
Takacs Robert <89.t.robert@gmail.com>
2020-05-18 13:47:15 +0200

    feat(sql-queries.js): added getByName() + changed naming of functions

--- js/sql-queries.js
+++ js/sql-queries.js
@@ -1,10 +1,15 @@
-const login = (name, password) => {
+const getByName = (name) => {
+    return `SELECT * FROM player WHERE name = '${name}'`;
+}
+
+const getByNameAndPassword = (name, password) => {
     return `SELECT * FROM player WHERE name = '${name}' AND password = '${password}'`;
 };
 
-const register = (name, password) => {
+const postNameAndPassword = (name, password) => {
     return `INSERT INTO player (name, password) VALUES ('${name}', '${password}')`;
 };
 
-exports.login = login;
-exports.register = register;
+exports.getByName = getByName;
+exports.getByNameAndPassword = getByNameAndPassword;
+exports.postNameAndPassword = postNameAndPassword;
