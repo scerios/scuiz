6e39fa5799795a4fb53e855cfe2255c8566da2ce
Takacs Robert <89.t.robert@gmail.com>
2020-05-18 16:40:16 +0200

    feat(error.js) error strings separated from server.js

---
+++ js/error.js
@@ -0,0 +1,8 @@
+const errors = {
+    standardError: 'Hiba történt',
+    namingError: 'Nem megfelelő név',
+    connectionIssue: 'Nem sikerült kapcsolódni a szerverhez, kérlek próbáld újra.',
+    alreadyRegistered: 'Ezzel a névvel már regisztráltak, válassz másikat!'
+}
+
+module.exports = errors;
