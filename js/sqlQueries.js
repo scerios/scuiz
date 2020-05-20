c40f0f8c467aec76944cedf069a616692b3d6f82
Takacs Robert <89.t.robert@gmail.com>
2020-05-20 13:51:48 +0200

    feat(sqlQueries.js): added getCategoryRoundLimit()

--- js/sqlQueries.js
+++ js/sqlQueries.js
@@ -14,7 +14,12 @@ const getAllCategories = () => {
     return 'SELECT id, name, question_index FROM category';
 };
 
+const getCategoryRoundLimit = () => {
+    return 'SELECT round_limit FROM round_count WHERE id = 1';
+}
+
 exports.getByName = getByName;
 exports.getByNameAndPassword = getByNameAndPassword;
 exports.postNameAndPassword = postNameAndPassword;
 exports.getAllCategories = getAllCategories;
+exports.getCategoryRoundLimit = getCategoryRoundLimit;
