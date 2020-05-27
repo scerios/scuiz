d4db6823108bb74c48301ab22703d7ca1dacf748
Takacs Robert <89.t.robert@gmail.com>
2020-05-27 11:29:44 +0200

    feat(player.js): added router.get('/') and language.js implementation

--- routes/players.js
+++ routes/players.js
@@ -1,5 +1,15 @@
 const EXPRESS = require('express');
 const ROUTER = EXPRESS.Router();
+const LANGUAGE = require('./../js/language');
 
+let language = LANGUAGE.getLanguage('hu');
+
+ROUTER.get('/', (req, res) => {
+    res.render('index', {
+        welcomeMsg: language.indexWelcomeMsg,
+        loginBtn: language.indexLoginBtn,
+        registerBtn: language.indexRegisterBtn
+    });
+});
 
 module.exports = ROUTER;
