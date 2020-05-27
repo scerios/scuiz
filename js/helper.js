b2c5cdd6db9028311c25cf728eb6864f8ac7c930
Takacs Robert <89.t.robert@gmail.com>
2020-05-27 15:11:53 +0200

    feat(helper.js): added tryGetInputErrors(inputs, errorMessages)

--- js/helper.js
+++ js/helper.js
@@ -5,4 +5,28 @@ function getCategoryAvailabilities(categories, limit) {
     return categories;
 }
 
+function tryGetInputErrors(inputs, errorMessages) {
+    let { name, password, confirmPassword} = inputs;
+    let errors = [];
+
+    if (!name || !password || !confirmPassword) {
+        errors.push(errorMessages.required);
+    }
+
+    if (name.length < 3) {
+        errors.push(errorMessages.tooShortName);
+    }
+
+    if (password.length < 6) {
+        errors.push(errorMessages.tooShortPassword);
+    }
+
+    if (password !== confirmPassword) {
+        errors.push(errorMessages.different);
+    }
+
+    return errors;
+}
+
 exports.getCategoryAvailabilities = getCategoryAvailabilities;
+exports.tryGetInputErrors = tryGetInputErrors;
