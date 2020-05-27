585cf2dda346591ac6c7dbe506023e004188bd7a
Takacs Robert <89.t.robert@gmail.com>
2020-05-27 13:11:12 +0200

    feat(players.js): added router.get('/register')

--- routes/players.js
+++ routes/players.js
@@ -6,9 +6,24 @@ let language = LANGUAGE.getLanguage('hu');
 
 ROUTER.get('/', (req, res) => {
     res.render('index', {
-        welcomeMsg: language.indexWelcomeMsg,
-        loginBtn: language.indexLoginBtn,
-        registerBtn: language.indexRegisterBtn
+        welcomeMsg: language.index.welcomeMsg,
+        loginBtn: language.index.loginBtn,
+        registerBtn: language.index.registerBtn
+    });
+});
+
+ROUTER.get('/register', (req, res) => {
+    res.render('register', {
+        welcomeMsg: language.register.welcomeMsg,
+        nameLabel: language.register.nameLabel,
+        namePlaceHolder: language.register.namePlaceHolder,
+        passwordLabel: language.register.passwordLabel,
+        passwordPlaceHolder: language.register.passwordPlaceHolder,
+        confirmPasswordLabel: language.register.confirmPasswordLabel,
+        confirmPasswordPlaceHolder: language.register.confirmPasswordPlaceHolder,
+        registerBtn: language.register.registerBtn,
+        isRegisteredQuestion: language.register.isRegisteredQuestion,
+        loginLink: language.register.loginLink
     });
 });
 
