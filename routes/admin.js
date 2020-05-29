68fcff728c1f2075fed56c5c3f1ff14462dad611
Takacs Robert <89.t.robert@gmail.com>
2020-05-29 17:09:42 +0200

    feat(admin.js): added route.get('/admin')

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,44 @@
 const EXPRESS = require('express');
 const ROUTER = EXPRESS.Router();
+const LANGUAGE = require('./../js/language');
+const SQL_QUERIES = require('./../js/sqlQueries');
 
+function getAdminLoginPage(language) {
+    return {
+        welcomeMsg: language.adminLogin.welcomeMsg,
+        nameLabel: language.adminLogin.nameLabel,
+        namePlaceholder: language.adminLogin.namePlaceholder,
+        passwordLabel: language.adminLogin.passwordLabel,
+        passwordPlaceholder: language.adminLogin.passwordPlaceholder,
+        loginBtn: language.adminLogin.loginBtn,
+    };
+}
+
+function getControlPanelPage(language) {
+    return {
+        welcomeMsg: language.adminLogin.welcomeMsg,
+        nameLabel: language.adminLogin.nameLabel,
+        namePlaceholder: language.adminLogin.namePlaceholder,
+        passwordLabel: language.adminLogin.passwordLabel,
+        passwordPlaceholder: language.adminLogin.passwordPlaceholder,
+        loginBtn: language.adminLogin.loginBtn,
+    };
+}
+
+ROUTER.get('/admin', (req, res) => {
+    let language = LANGUAGE.getLanguage(req.session.language);
+
+    if (req.session.adminId !== undefined) {
+        let gameBoard = getControlPanelPage(language);
+        res.render('control-panel', {
+            gameBoard
+        });
+    } else {
+        let adminLogin = getAdminLoginPage(language);
+        res.render('admin-login', {
+            adminLogin
+        });
+    }
+});
 
 module.exports = ROUTER;
