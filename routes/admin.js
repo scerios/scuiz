859e26e8cee41c6ffd5038a2cf7a9fe8c17390c8
Takacs Robert <89.t.robert@gmail.com>
2020-05-29 18:23:00 +0200

    feat(): added basic template for control panel with style

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ const EXPRESS = require('express');
 const ROUTER = EXPRESS.Router();
 const LANGUAGE = require('./../js/language');
 const SQL_QUERIES = require('./../js/sqlQueries');
+const HELPER = require('./../js/helper');
 
 function getAdminLoginPage(language) {
     return {
@@ -14,14 +15,10 @@ function getAdminLoginPage(language) {
     };
 }
 
-function getControlPanelPage(language) {
+function getControlPanelPage(language, categories, players) {
     return {
-        welcomeMsg: language.adminLogin.welcomeMsg,
-        nameLabel: language.adminLogin.nameLabel,
-        namePlaceholder: language.adminLogin.namePlaceholder,
-        passwordLabel: language.adminLogin.passwordLabel,
-        passwordPlaceholder: language.adminLogin.passwordPlaceholder,
-        loginBtn: language.adminLogin.loginBtn,
+        categories: categories,
+        players: players
     };
 }
 
@@ -29,10 +26,7 @@ ROUTER.get('/admin', (req, res) => {
     let language = LANGUAGE.getLanguage(req.session.language);
 
     if (req.session.adminId !== undefined) {
-        let controlPanel = getControlPanelPage(language);
-        res.render('control-panel', {
-            controlPanel
-        });
+        renderControlPanel(res, language);
     } else {
         let adminLogin = getAdminLoginPage(language);
         res.render('admin-login', {
@@ -42,7 +36,6 @@ ROUTER.get('/admin', (req, res) => {
 });
 
 ROUTER.post('/adminLogin', (req, res) => {
-    let language = LANGUAGE.getLanguage(req.session.language);
     let { name, password } = req.body;
     let adminLoginResult = SQL_QUERIES.getAdminPasswordByName(name);
 
@@ -51,15 +44,61 @@ ROUTER.post('/adminLogin', (req, res) => {
             if (password === admin[0].password) {
                 req.session.adminId = 1;
                 req.session.adminName = name;
+                res.redirect('/controlPanel');
+            } else  {
+                res.redirect('/admin');
             }
+        } else {
+            res.redirect('/admin');
         }
 
-        res.redirect('/admin');
     }).catch((error) => {
         console.log('playerLoginResult: ' + error);
 
         res.redirect('/admin');
     });
 });
+
+ROUTER.get('/controlPanel', (req, res) => {
+    let language = LANGUAGE.getLanguage(req.session.language);
+
+    if (req.session.adminId !== undefined) {
+        renderControlPanel(res, language);
+    } else {
+        let adminLogin = getAdminLoginPage(language);
+        res.render('admin-login', {
+            adminLogin
+        });
+    }
+});
+
+function renderControlPanel(res, language) {
+    let categoryResult = SQL_QUERIES.getAllCategories();
+
+    categoryResult.then((categories) => {
+        let categoryRoundLimitResult = SQL_QUERIES.getCategoryRoundLimit();
+
+        categoryRoundLimitResult.then((roundLimit) => {
+            let sortedCategories = HELPER.getCategoryAvailabilities(categories, roundLimit[0].round_limit);
+            let playersResult = SQL_QUERIES.getAllLoggedInPlayers();
+
+            playersResult.then((players) => {
+                let controlPanel = getControlPanelPage(language, sortedCategories, players);
+                res.render('control-panel', {
+                    controlPanel
+                });
+
+            }).catch((error) => {
+                console.log('questionsResult: ' + error);
+            });
+
+        }).catch((error) => {
+            console.log('categoryRoundLimitResult: ' + error);
+        });
+
+    }).catch((error) => {
+        console.log('categoryResult: ' + error);
+    });
+}
 
 module.exports = ROUTER;
