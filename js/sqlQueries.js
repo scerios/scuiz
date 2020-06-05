0ba452f833c953b2f4990f2c47bc9fcd377d6bad
Takacs Robert <89.t.robert@gmail.com>
2020-06-05 16:27:54 +0200

    refactor(sqlQueries.js): append all async function name with Async

--- js/sqlQueries.js
+++ js/sqlQueries.js
@@ -2,19 +2,19 @@ const mySql = require('./mySqlConnection');
 const util = require('util');
 const query = util.promisify(mySql.query).bind(mySql);
 
-async function postPlayer(name, password) {
+async function postPlayerAsync(name, password) {
     return await query(`INSERT INTO player (name, password) VALUES ('${name}', '${password}')`);
 }
 
-async function putPlayerStatusById(id, status) {
+async function putPlayerStatusByIdAsync(id, status) {
     return await query(`UPDATE player SET status = ${status} WHERE id = ${id}`);
 }
 
-async function putPlayerSocketIdById(id, socketId) {
+async function putPlayerSocketIdByIdAsync(id, socketId) {
     return await query(`UPDATE player SET socket_id = '${socketId}' WHERE id = ${id}`);
 }
 
-async function putPlayerStatusAndSocketIdBySocketId(socketId, status) {
+async function putPlayerStatusAndSocketIdBySocketIdAsync(socketId, status) {
     return await query(`UPDATE player SET status = ${status}, socket_id = '' WHERE socket_id = '${socketId}'`);
 }
 
@@ -22,35 +22,35 @@ function putPlayerPointAddTwoById(id) {
     mySql.query(`UPDATE player SET point = point + 2 WHERE id = ${id}`);
 }
 
-async function getAllLoggedInPlayers() {
+async function getAllLoggedInPlayersAsync() {
     return await query('SELECT id, socket_id, name, point FROM player WHERE status = 1');
 }
 
-async function getPlayerById(id) {
+async function getPlayerByIdAsync(id) {
     return await query(`SELECT id, socket_id, name, point, status FROM player WHERE id = '${id}'`);
 }
 
-async function getPlayerByName(name) {
+async function getPlayerByNameAsync(name) {
     return await query(`SELECT id, password, status FROM player WHERE name = '${name}'`);
 }
 
-async function putCategoryQuestionIndexById(id, index) {
+async function putCategoryQuestionIndexByIdAsync(id, index) {
     return await query(`UPDATE category SET question_index = ${index} WHERE id = ${id}`);
 }
 
-async function getAllCategories() {
+async function getAllCategoriesAsync() {
     return await query('SELECT id, name, question_index FROM category');
 }
 
-async function getCategoryRoundLimit() {
+async function getCategoryRoundLimitAsync() {
     return await query('SELECT round_limit FROM category_limit WHERE id = 1');
 }
 
-async function getAdminPasswordByName(name) {
+async function getAdminPasswordByNameAsync(name) {
     return await query(`SELECT password FROM admin WHERE name = '${name}'`);
 }
 
-async function getAllQuestions() {
+async function getAllQuestionsAsync() {
     return await query(
         'SELECT c.name, q.question, q.answer FROM question q ' +
         'INNER JOIN category c ' +
@@ -58,7 +58,7 @@ async function getAllQuestions() {
     );
 }
 
-async function getQuestionByCategoryIdAndQuestionIndex(categoryId, index) {
+async function getQuestionByCategoryIdAndQuestionIndexAsync(categoryId, index) {
     index--;
     return await query(
             `SELECT q.question, q.answer, c.name FROM question q ` +
@@ -67,23 +67,23 @@ async function getQuestionByCategoryIdAndQuestionIndex(categoryId, index) {
             `WHERE category_id = ${categoryId} LIMIT ${index}, 1`);
 }
 
-async function putCategoryLimit(limit) {
+async function putCategoryLimitAsync(limit) {
     limit += 3;
     return await query(`UPDATE category_limit SET round_limit = ${limit} WHERE id = 1`);
 }
 
-exports.postPlayer = postPlayer;
-exports.putPlayerStatusById = putPlayerStatusById;
-exports.putPlayerSocketIdById = putPlayerSocketIdById;
-exports.putPlayerStatusAndSocketIdBySocketId = putPlayerStatusAndSocketIdBySocketId;
+exports.postPlayerAsync = postPlayerAsync;
+exports.putPlayerStatusByIdAsync = putPlayerStatusByIdAsync;
+exports.putPlayerSocketIdByIdAsync = putPlayerSocketIdByIdAsync;
+exports.putPlayerStatusAndSocketIdBySocketIdAsync = putPlayerStatusAndSocketIdBySocketIdAsync;
 exports.putPlayerPointAddTwoById = putPlayerPointAddTwoById;
-exports.getAllLoggedInPlayers = getAllLoggedInPlayers;
-exports.getPlayerById = getPlayerById;
-exports.getPlayerByName = getPlayerByName;
-exports.putCategoryQuestionIndexById = putCategoryQuestionIndexById;
-exports.getAllCategories = getAllCategories;
-exports.getCategoryRoundLimit = getCategoryRoundLimit;
-exports.getAdminPasswordByName = getAdminPasswordByName;
-exports.getAllQuestions = getAllQuestions;
-exports.getQuestionByCategoryIdAndQuestionIndex = getQuestionByCategoryIdAndQuestionIndex;
-exports.putCategoryLimit = putCategoryLimit;
+exports.getAllLoggedInPlayersAsync = getAllLoggedInPlayersAsync;
+exports.getPlayerByIdAsync = getPlayerByIdAsync;
+exports.getPlayerByNameAsync = getPlayerByNameAsync;
+exports.putCategoryQuestionIndexByIdAsync = putCategoryQuestionIndexByIdAsync;
+exports.getAllCategoriesAsync = getAllCategoriesAsync;
+exports.getCategoryRoundLimitAsync = getCategoryRoundLimitAsync;
+exports.getAdminPasswordByNameAsync = getAdminPasswordByNameAsync;
+exports.getAllQuestionsAsync = getAllQuestionsAsync;
+exports.getQuestionByCategoryIdAndQuestionIndexAsync = getQuestionByCategoryIdAndQuestionIndexAsync;
+exports.putCategoryLimitAsync = putCategoryLimitAsync;
